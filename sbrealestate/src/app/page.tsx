1a7c2d32507631da8429e8ea7435fa3468ba3b08
Sumit Thakur <sumit@thexyzstudio.com>
2025-02-18 12:36:05 +0530

    initialize project structure with Next.js, Tailwind CSS, and essential components

---
+++ sbrealestate/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="max-w-screen-2xl mx-auto overflow-hidden">
+
+    </div>
+  );
+}
---
+++ sbrealestate/tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        primary: "var(--primary)",
+       Black: "var(--black)",
+       Green: "var(--green)",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
